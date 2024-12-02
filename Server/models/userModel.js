22d404fc0c798a2cf2554f3671b0b775073414ee
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-12-02 10:47:30 +0530

    user model

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -18,6 +18,10 @@ const userModel = new mongoose.Schema({
         type: String,
         required:true
     },
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
     role: {
         type: String,
         enum: ['admin', 'user'],
