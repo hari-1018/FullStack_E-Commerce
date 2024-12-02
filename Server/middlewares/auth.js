1c0549ee6cf9b900a42c585a681c87fa3010e2dd
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-12-02 10:45:58 +0530

    auth-user verification

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -2,18 +2,29 @@ const asyncErrorResolver = require('./asyncErrorResolver');
 const CustomError = require('../utils/customErrors')
 const { verifyTokenGenerated } = require('../utils/jwt');
 
-const authorize = asyncErrorResolver(async (req,res,next)=>{
+const auth = asyncErrorResolver(async (req,res,next)=>{
     const token = req.headers['authorization']?.replace("Bearer ","")
     if(!token){
         throw new CustomError("Authorization denied.Token is required", 401);
     }
-    const decodedToken = verifyTokenGenerated(token);
 
+    try{
+    const decodedToken = verifyTokenGenerated(token);
     if(!decodedToken || !decodedToken.id){
         throw new CustomError("Invalid Token. Try again", 401);
     }
+
+    if (!decodedToken.role) {
+        throw new CustomError("Token does not contain role information.", 401);
+    }
+
     req.user = decodedToken;
     next();
+    }catch(error){
+        console.error("Auth error", error.message);
+        throw new CustomError("Invalid Token. Try again", 401)
+    }
+
 });
 
-module.exports = authorize;
+module.exports = auth;
